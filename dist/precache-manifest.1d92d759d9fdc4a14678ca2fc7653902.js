b5b69ddb6f345c4d7dcaf71438298846bb656310
Viktor Kuzhelny <viktor@marketto.ru>
2019-07-29 13:09:11 +0500

    Publish live preview

---
+++ dist/precache-manifest.1d92d759d9fdc4a14678ca2fc7653902.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "666e135393ac4ac62ae551b9fb0985ae",
+    "url": "/fonts/materialdesignicons-webfont.666e1353.eot"
+  },
+  {
+    "revision": "b76eb8dd473607290ea874f8ef33983c",
+    "url": "/data-sources/avatars/nicolas-horn-689011-unsplash.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "82125a6051cb90f30965",
+    "url": "/js/chunk-vendors.bf020de3.js"
+  },
+  {
+    "revision": "020c53784325c471713a",
+    "url": "/js/forms.89aaa59e.js"
+  },
+  {
+    "revision": "1492dbef2db58b4d2a74",
+    "url": "/js/profile.590ffece.js"
+  },
+  {
+    "revision": "bf4f2bd050e497017bcf",
+    "url": "/js/tables.c389f155.js"
+  },
+  {
+    "revision": "7298fabbecbc6f87cf690f717af8652c",
+    "url": "/img/justboil-logo.7298fabb.svg"
+  },
+  {
+    "revision": "815a2673d08221fb4968f19e59a14f2e",
+    "url": "/fonts/materialdesignicons-webfont.815a2673.woff2"
+  },
+  {
+    "revision": "875141d94aca807fbf74f7f53fa98a26",
+    "url": "/fonts/materialdesignicons-webfont.875141d9.woff"
+  },
+  {
+    "revision": "e06ef6878791ff7db6c2",
+    "url": "/js/app.6917db29.js"
+  },
+  {
+    "revision": "f558fb5cc12a31989a7e4048b4fba71a",
+    "url": "/fonts/materialdesignicons-webfont.f558fb5c.ttf"
+  },
+  {
+    "revision": "54d1720c5c785c80de38ed501e86d893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c761978a7f41bc4a67db1179b4acbc09",
+    "url": "/data-sources/clients.json"
+  },
+  {
+    "revision": "924a58fb4e40f8bec6d5eabce2947744",
+    "url": "/data-sources/avatars/joshua-rawson-harris-432289-unsplash.jpg"
+  },
+  {
+    "revision": "16a2e6c8d9505fd5d3cd9a42362f3ddc",
+    "url": "/data-sources/avatars/freestocks-org-190062-unsplash.jpg"
+  },
+  {
+    "revision": "48f7f75c2e775c5004ce0886e5e6c5e2",
+    "url": "/data-sources/avatars/benjamin-parker-699522-unsplash.jpg"
+  },
+  {
+    "revision": "a6f1f670e35e2b4521eeb0c04de5b4c2",
+    "url": "/data-sources/avatars/annie-spratt-121576-unsplash.jpg"
+  },
+  {
+    "revision": "e6b7e524fc15f61b8033c382f1f7e0da",
+    "url": "/data-sources/avatars/andy-lee-642320-unsplash.jpg"
+  },
+  {
+    "revision": "82125a6051cb90f30965",
+    "url": "/css/chunk-vendors.3e0dee21.css"
+  },
+  {
+    "revision": "e06ef6878791ff7db6c2",
+    "url": "/css/app.fc348326.css"
+  }
+];
