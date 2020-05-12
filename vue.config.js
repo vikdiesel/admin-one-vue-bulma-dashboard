89fb3ea63a61b01b920f037359f7309e7b7ba69d
Viktor Kuzhelny <viktor@marketto.ru>
2020-05-12 14:52:10 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   publicPath: process.env.DEPLOY_ENV === 'GH_PAGES'
     ? '/admin-one-vue-bulma-dashboard/'
     : '/',
+
+  // Remove moment.js from chart.js
+  // https://www.chartjs.org/docs/latest/getting-started/integration.html#bundlers-webpack-rollup-etc
   configureWebpack: config => {
     return {
       externals: {
