62be19fb9a5faae566ab736bb6b12363730ab6de
vikdiesel <viktor@marketto.ru>
2021-11-17 16:39:33 +0000

    Deploying to vue-2x-demo from @ vikdiesel/admin-one-vue-bulma-dashboard@600e2aa34ad3657f0d7f8e4e837b4da7ba4d52e3 🚀

---
+++ precache-manifest.17c8a82d91dc851de113b57d7a73e423.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216eef7a6ac7b9c48ca0",
+    "url": "/admin-one-vue-bulma-dashboard/css/app.1744d5fa.css"
+  },
+  {
+    "revision": "0e480abdb7a35c2e9b8571d68187b4d7",
+    "url": "/admin-one-vue-bulma-dashboard/data-sources/clients.json"
+  },
+  {
+    "revision": "9ed33b873e49a44feb61a3e2fea0dc2a",
+    "url": "/admin-one-vue-bulma-dashboard/favicon.png"
+  },
+  {
+    "revision": "7298fabbecbc6f87cf690f717af8652c",
+    "url": "/admin-one-vue-bulma-dashboard/img/justboil-logo.7298fabb.svg"
+  },
+  {
+    "revision": "b0446335cc40ffdc673f78811342c23e",
+    "url": "/admin-one-vue-bulma-dashboard/index.html"
+  },
+  {
+    "revision": "216eef7a6ac7b9c48ca0",
+    "url": "/admin-one-vue-bulma-dashboard/js/app.a58051b9.js"
+  },
+  {
+    "revision": "e7cb5b3d1acab4f09e3e",
+    "url": "/admin-one-vue-bulma-dashboard/js/chunk-vendors.353429ad.js"
+  },
+  {
+    "revision": "7454f87d7857b70e542f",
+    "url": "/admin-one-vue-bulma-dashboard/js/client-form.3c2c56bc.js"
+  },
+  {
+    "revision": "dc09155142f68415a9b1",
+    "url": "/admin-one-vue-bulma-dashboard/js/client-form~forms.1f0fd972.js"
+  },
+  {
+    "revision": "7d93c5298da10f9d2bb7",
+    "url": "/admin-one-vue-bulma-dashboard/js/forms.87662115.js"
+  },
+  {
+    "revision": "31ec4e15c9ec513cca83",
+    "url": "/admin-one-vue-bulma-dashboard/js/profile.06cc8e13.js"
+  },
+  {
+    "revision": "e54503e889ec3264a4c4",
+    "url": "/admin-one-vue-bulma-dashboard/js/tables.91e5749a.js"
+  },
+  {
+    "revision": "9e02268cb14385af9653507bc8b8ee72",
+    "url": "/admin-one-vue-bulma-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin-one-vue-bulma-dashboard/robots.txt"
+  }
+]);
