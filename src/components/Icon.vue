5714e542d5826f206a0dac53db6ec28ac8c40367
Viktor Kuzhelnyi <viktor@marketto.ru>
2021-12-02 17:48:57 +0500

    fix division

---
+++ src/components/Icon.vue
@@ -0,0 +1,39 @@
+<template>
+  <span class="icon">
+    <svg
+      viewBox="0 0 24 24"
+      :width="size"
+      :height="size"
+      class="inline-block"
+    >
+      <path
+        fill="currentColor"
+        :d="path"
+      />
+    </svg>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    path: {
+      type: String,
+      required: true
+    },
+    w: {
+      type: String,
+      default: 'w-6'
+    },
+    h: {
+      type: String,
+      default: 'h-6'
+    },
+    size: {
+      type: [String, Number],
+      default: 16
+    }
+  }
+}
+</script>
