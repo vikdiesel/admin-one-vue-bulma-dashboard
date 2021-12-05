fa1bbd33bc526c701fc53b1adaeabe5a9942358c
Viktor Kuzhelnyi <viktor@marketto.ru>
2021-12-05 15:17:32 +0500

    remove commented stuff

--- src/views/full-page/Login.vue
+++ src/views/full-page/Login.vue
@@ -104,24 +104,5 @@ export default {
       submit
     }
   }
-  // data () {
-  //   return {
-  //     isLoading: false,
-  //     form: {
-  //       email: 'john.doe@example.com',
-  //       password: 'my-secret-password-9e9w',
-  //       remember: false
-  //     }
-  //   }
-  // },
-  // methods: {
-  //   submit () {
-  //     this.isLoading = true
-  //     setTimeout(() => {
-  //       this.isLoading = false
-  //       this.$router.push('/')
-  //     }, 750)
-  //   }
-  // }
 }
 </script>
